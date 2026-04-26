d8ec17fceb7fe3f8140ddc04c522d6f43476790e
Joan Sanchez <jsm37@alumnes.udl.cat>
2026-04-26 18:09:21 +0200

    t1.3 done

--- app/app/src/main/java/com/example/voyagetime/data/local/VoyageTimeDatabase.kt
+++ app/app/src/main/java/com/example/voyagetime/data/local/VoyageTimeDatabase.kt
@@ -3,9 +3,11 @@ package com.example.voyagetime.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.example.voyagetime.utils.RoomTypeConverters
+import com.example.voyagetime.data.local.dao.ItineraryItemDao
+import com.example.voyagetime.data.local.dao.TripDao
 import com.example.voyagetime.data.local.entity.ItineraryItemEntity
 import com.example.voyagetime.data.local.entity.TripEntity
+import com.example.voyagetime.utils.RoomTypeConverters
 
 @Database(
     entities = [
@@ -17,6 +19,10 @@ import com.example.voyagetime.data.local.entity.TripEntity
 )
 @TypeConverters(RoomTypeConverters::class)
 abstract class VoyageTimeDatabase : RoomDatabase() {
+
+    abstract fun tripDao(): TripDao
+    abstract fun itineraryItemDao(): ItineraryItemDao
+
     companion object {
         const val DATABASE_NAME = "voyagetime.db"
     }
